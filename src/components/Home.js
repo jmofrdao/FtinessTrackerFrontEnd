3a7c169252af5b738a8913e6eeb8b9cc0c91a3fd
Scott <wang.scottw@gmail.com>
2022-08-07 05:23:17 -0500

    Worked on CSS, AddActivity and Home. Will finish rest tomorrow

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import {useNavigate} from "react-router-dom";
+import "./css/Home.css";
 
 
 
 const Home = () => {
     return (
-        <div>
-            <h1>Welcome to Fitness Tracker</h1>
+        <div className="homeDiv">
+            <h1 className="homeTitle">Welcome to Fitness Tracker</h1>
         </div>
     )
 }
